ca811ec42458df9c074f9be4780328289ad8d8a8
vonrosen <hunter.stern@fundingcircle.com>
2017-11-13 11:51:28 -0800

    More changes

--- src/binomial/core.clj
+++ src/binomial/core.clj
@@ -5,7 +5,7 @@
 (def defaultu 1.4) ;;up factor
 (def defaultd 0.6) ;;down factor
 (def defaultr 0.05) ;; risk free rate of return
-(def default-total-time-periods 3) ;;levels in binomial tree, not including root
+(def default-total-time-periods 2) ;;levels in binomial tree, not including root
 (def total-time-periods default-total-time-periods)
 
 (defn discount-factor [r t]  
@@ -57,8 +57,10 @@
         (and (not (nil? (:payoff (:left tree))))
              (not (nil? (:payoff (:right tree))))))
     (let [new-tree (assoc tree :payoff (calculate-payoff r t q (:payoff (:left tree)) (:payoff (:right tree))))]
-      (prn (:payoff (:left tree)))
-      (prn (:payoff (:right tree)))      
+      ;(prn (:payoff (:left tree)))
+      ;(prn (:payoff (:right tree)))
+      ;(clojure.pprint/pprint new-tree)
+      ;;BUG! must take new-tree and put it back into original tree      
       (calculate-root-c new-tree r t q))
     (do 
       (if (nil? (:payoff (:left tree)))
@@ -74,5 +76,5 @@
       (calculate-c binomial-tree X strike-price r t q u d)))
   ([binomial-tree X strike-price r t q u d]
     (let [binomial-tree-with-leaves-populated (calculate-leaves binomial-tree X strike-price u d 0 0)]
-      (prn binomial-tree-with-leaves-populated)      
+      (clojure.pprint/pprint binomial-tree-with-leaves-populated)      
       (:payoff (calculate-root-c binomial-tree-with-leaves-populated r t q)))))
