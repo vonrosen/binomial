56e9bcc5695517e009d26ee3d04899a3296ca8dd
vonrosen <hunter.stern@fundingcircle.com>
2017-11-13 13:38:50 -0800

    More changes

--- src/binomial/core.clj
+++ src/binomial/core.clj
@@ -5,18 +5,19 @@
 (def defaultu 1.4) ;;up factor
 (def defaultd 0.6) ;;down factor
 (def defaultr 0.05) ;; risk free rate of return
-(def default-total-time-periods 2) ;;levels in binomial tree, not including root
+(def default-total-time-periods 3) ;;levels in binomial tree, not including root
 (def total-time-periods default-total-time-periods)
 
 (defn discount-factor [r t]  
   (Math/pow Math/E (* -1 r t)))
 
-(def calculate-q (memoize 
-                   (fn [r t u d]
-                     (/ (- (discount-factor r t) d) (- u d)))))
+(def calculate-q 
+  (memoize 
+   (fn [r t u d]     
+     (/ (- (Math/pow Math/E (* -1 r t)) d) (- u d)))))
 
 (defn build-empty-tree []
-  {:left nil :right nil :payoff nil :id nil})
+  {:left nil :right nil :payoff nil})
 
 (defn build-tree [tree time-periods]
   (let [new-tree (assoc tree :left (build-empty-tree) :right (build-empty-tree))]
@@ -56,25 +57,21 @@
         (nil? (:payoff tree))
         (and (not (nil? (:payoff (:left tree))))
              (not (nil? (:payoff (:right tree))))))
-    (let [new-tree (assoc tree :payoff (calculate-payoff r t q (:payoff (:left tree)) (:payoff (:right tree))))]
-      ;(prn (:payoff (:left tree)))
-      ;(prn (:payoff (:right tree)))
-      ;(clojure.pprint/pprint new-tree)
-      ;;BUG! must take new-tree and put it back into original tree      
-      (calculate-root-c new-tree r t q))
+    (assoc tree :payoff (calculate-payoff r t q (:payoff (:left tree)) (:payoff (:right tree))))
     (do 
       (if (nil? (:payoff (:left tree)))
-        (calculate-root-c (:left tree) r t q) 
-        (if (nil? (:payoff (:right tree)))
-          (calculate-root-c (:right tree) r t q)
-          tree)))))
+        (let [new-tree (assoc tree :left (calculate-root-c (:left tree) r t q))]
+          (if (nil? (:payoff (:right new-tree)))
+            (let [new-tree (assoc new-tree :right (calculate-root-c (:right new-tree) r t q))]
+              (calculate-root-c new-tree r t q))                                  
+            tree))))))
 
 (defn calculate-c 
-  ([X strike-price r t u d]
+  ([X strike-price r t u d n]
     (let [q (calculate-q r t u d)
-          binomial-tree (build-tree (build-empty-tree) default-total-time-periods)]
+          binomial-tree (build-tree (build-empty-tree) n)]
       (calculate-c binomial-tree X strike-price r t q u d)))
   ([binomial-tree X strike-price r t q u d]
     (let [binomial-tree-with-leaves-populated (calculate-leaves binomial-tree X strike-price u d 0 0)]
-      (clojure.pprint/pprint binomial-tree-with-leaves-populated)      
+      (clojure.pprint/pprint (calculate-root-c binomial-tree-with-leaves-populated r t q))      
       (:payoff (calculate-root-c binomial-tree-with-leaves-populated r t q)))))
