0bd21073e050255bc90e2a1d01129b6047443c09
Hunter Stern <hunter.stern@autodesk.com>
2018-11-18 19:32:03 -0800

    Add cider and lein eclipse plugins

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  #_:plugins #_[[cider/cider-nrepl "0.9.1"]]
+  :plugins [[cider/cider-nrepl "0.9.1"]
+	      [lein2-eclipse "2.0.0"]]
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
   #_:profiles #_{:dev {:dependencies [[com.cemerick/piggieback "0.2.0"]
